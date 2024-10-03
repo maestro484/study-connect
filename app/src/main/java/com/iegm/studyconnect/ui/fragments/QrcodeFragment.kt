753d438ea6abf8af8c6223343cdce413f463c899
estudiante <valen.301006@gmail.com>
2024-10-03 08:54:30 -0500

    Todo good a segun www

--- app/src/main/java/com/iegm/studyconnect/ui/fragments/QrcodeFragment.kt
+++ app/src/main/java/com/iegm/studyconnect/ui/fragments/QrcodeFragment.kt
@@ -1,12 +1,15 @@
 package com.iegm.studyconnect.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import com.iegm.studyconnect.AppTheme
 import com.iegm.studyconnect.MainActivity
 import com.iegm.studyconnect.R
 
@@ -14,8 +17,7 @@ import com.iegm.studyconnect.R
 class QrcodeFragment : Fragment() {
 
     lateinit var back: ImageView
-
-
+    lateinit var topBar: ConstraintLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,14 +25,15 @@ class QrcodeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_qrcode, container, false)
-
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         back = view.findViewById(R.id.back)
+
+        topBar.setBackgroundColor(Color.parseColor(AppTheme.obtenerTema(requireActivity())))
+
 
 
         back.setOnClickListener {
