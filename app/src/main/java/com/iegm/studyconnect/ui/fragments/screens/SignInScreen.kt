80cf3157a7a78d208585cb3bd668b2786d5bcf9d
estudiante <juanmg777vg@gmail.com>
2024-11-05 10:47:58 -0500

    Todo login listo y las flechas

--- app/src/main/java/com/iegm/studyconnect/ui/fragments/screens/SignInScreen.kt
+++ app/src/main/java/com/iegm/studyconnect/ui/fragments/screens/SignInScreen.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
@@ -32,7 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -41,7 +38,6 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.iegm.studyconnect.AuthViewModel
@@ -55,17 +51,17 @@ fun SignInScreen(navHostController: NavHostController, authViewModel: AuthViewMo
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var isButtonEnabled by remember { mutableStateOf(false) }
+
     // Listener para habilitar el botón
-    LaunchedEffect( email, password) {
-        isButtonEnabled =
-            email.isNotEmpty() && password.isNotEmpty()
+    LaunchedEffect(email, password) {
+        isButtonEnabled = email.isNotEmpty() && password.isNotEmpty()
     }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-
         Image(
             modifier = Modifier.size(250.dp),
             painter = painterResource(id = R.drawable.login_image),
@@ -74,83 +70,43 @@ fun SignInScreen(navHostController: NavHostController, authViewModel: AuthViewMo
         Spacer(modifier = Modifier.height(20.dp))
 
         Text(text = "¡BIENVENIDO!", fontSize = 35.sp, fontWeight = FontWeight.Bold)
-        var email by remember {
-            mutableStateOf("")
-        }
 
-        OutlinedTextField(value = email, onValueChange = {
-            email = it
-        }, label = {
-            Text(text = "Email")
-        }, placeholder = {
-            Text(text = "Ingresa tu email.")
-
-        }, leadingIcon = {
-            Icon(imageVector = Icons.Filled.Email, contentDescription = "Icono del correo.")
-        }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email))
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(text = "Email") },
+            placeholder = { Text(text = "Ingresa tu email.") },
+            leadingIcon = { Icon(imageVector = Icons.Filled.Email, contentDescription = "Icono del correo.") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
 
         Spacer(modifier = Modifier.height(20.dp))
 
-
-        var password by remember {
-            mutableStateOf("")
-        }
-        var showPassword by remember {
-
-            mutableStateOf(false)
-        }
-        OutlinedTextField(value = password,
-            onValueChange = {
-                password = it
-            },
-            label = {
-                Text(text = "Contraseña")
-            },
-            placeholder = {
-                Text(text = "Ingresa su contraseña.")
-
-            },
-            leadingIcon = {
-                Icon(imageVector = Icons.Filled.Lock, contentDescription = "Icono de candado.")
-            },
+        var showPassword by remember { mutableStateOf(false) }
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "Contraseña") },
+            placeholder = { Text(text = "Ingresa su contraseña.") },
+            leadingIcon = { Icon(imageVector = Icons.Filled.Lock, contentDescription = "Icono de candado.") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             visualTransformation = if (showPassword) {
                 VisualTransformation.None
             } else {
-
                 PasswordVisualTransformation()
             },
             trailingIcon = {
-                if (showPassword) {
-                    IconButton(onClick = { showPassword = false }) {
-                        Icon(
-                            imageVector = Icons.Filled.Visibility,
-                            contentDescription = "Icono mostrar contraseña"
-                        )
-
-                    }
-                } else {
-                    IconButton(onClick = { showPassword = true }) {
-                        Icon(
-                            imageVector = Icons.Filled.VisibilityOff,
-                            contentDescription = "Icono ocultar contraseña"
-                        )
-
-                    }
-
+                IconButton(onClick = { showPassword = !showPassword }) {
+                    Icon(
+                        imageVector = if (showPassword) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
+                        contentDescription = if (showPassword) "Icono mostrar contraseña" else "Icono ocultar contraseña"
+                    )
                 }
             }
         )
 
         Spacer(modifier = Modifier.height(20.dp))
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
 
-        ) {
-
-
-        }
         Text(text = "¿No tienes cuenta?", fontSize = 15.sp, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.width(4.dp))
         Text(
@@ -162,15 +118,13 @@ fun SignInScreen(navHostController: NavHostController, authViewModel: AuthViewMo
         )
         Spacer(modifier = Modifier.height(20.dp))
 
-
         Button(
             modifier = Modifier.width(250.dp),
             colors = ButtonDefaults.buttonColors(Color.Black),
-            onClick = {if (isButtonEnabled) authViewModel.SignIn(email, password)},
-            enabled = isButtonEnabled  // habilitar o deshabilitar el botón
-                ){
-
-            Text(text = "Iniciar sesión",color = Color.White)
+            onClick = { if (isButtonEnabled) authViewModel.SignIn(email, password) },
+            enabled = isButtonEnabled
+        ) {
+            Text(text = "Iniciar sesión", color = Color.White)
         }
     }
 }
